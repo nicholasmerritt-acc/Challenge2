a6bb3503596d6b3eb277747e9483b0f3233166d2
nicholasmerritt-acc <nicholas.merritt@g.austincc.edu>
2026-06-14 22:59:45 -0500

    all required fixes done
    random ball is now generated instead of dog
    dog is spawned on spacebar press instead of ball
    dog collider is now size of dog instead of size of screen
    when ball hits the ground (<-5) display Game Over
    correct bounds checking to destroy objects when they leave the screen
    random spawn timer adds 0-2 seconds

--- Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnManagerX : MonoBehaviour
@@ -23,10 +22,17 @@ public class SpawnManagerX : MonoBehaviour
     void SpawnRandomBall ()
     {
         // Generate random ball index and random spawn position
-        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+        Vector3 spawnPosition = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+        int spawnIndex = Random.Range(0, ballPrefabs.Length);
 
-        // instantiate ball at random spawn location
-        Instantiate(ballPrefabs[0], spawnPos, ballPrefabs[0].transform.rotation);
+        IEnumerator WaitRandomSeconds()
+        {
+            yield return new WaitForSeconds(Random.Range(0f, 2.0f));
+            // instantiate ball at random spawn location
+            Instantiate(ballPrefabs[spawnIndex], spawnPosition, ballPrefabs[spawnIndex].transform.rotation);
+        }
+
+        StartCoroutine(WaitRandomSeconds());
     }
 
 }
