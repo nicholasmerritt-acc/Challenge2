e182d971dd2e57b27d7544c9f1d7b45b7f61cf1d
nicholasmerritt-acc <nicholas.merritt@g.austincc.edu>
2026-06-14 23:12:52 -0500

    add .5 sec timeout so player can't spam spawn dogs
    also simplify spawn repeating code

--- Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -5,20 +5,32 @@ public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
     public InputAction fireAction;
+    public float dogSpawnTimeout;
+    private float dogSpawnTimeoutAmount = 0.5f;
 
     // Start is called before the first frame update
     void Start()
     {
         fireAction.Enable();
+        dogSpawnTimeout = 0.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        // On spacebar press, send dog
-        if (fireAction.triggered)
+        // On spacebar press, send dog if timeout is over
+        if (dogSpawnTimeout <= 0 )
         {
-            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            dogSpawnTimeout = 0;
+            if (fireAction.triggered)
+            {
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+                dogSpawnTimeout = dogSpawnTimeoutAmount;
+            }
+        }
+        else //time player out so they don't spam dogs
+        {
+            dogSpawnTimeout -= Time.deltaTime;
         }
     }
 }
--- Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -1,21 +1,21 @@
-﻿using System.Collections;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class SpawnManagerX : MonoBehaviour
 {
     public GameObject[] ballPrefabs;
 
-    private float spawnLimitXLeft = -22;
-    private float spawnLimitXRight = 7;
+    private float spawnLimitXLeft = -25;
+    private float spawnLimitXRight = 12;
     private float spawnPosY = 30;
 
     private float startDelay = 1.0f;
-    private float spawnInterval = 4.0f;
+    private float spawnIntervalMin = 3.0f;
+    private float spawnIntervalMax = 5.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+        Invoke(nameof(SpawnRandomBall), startDelay);
     }
 
     // Spawn random ball at random x position at top of play area
@@ -25,14 +25,9 @@ public class SpawnManagerX : MonoBehaviour
         Vector3 spawnPosition = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
         int spawnIndex = Random.Range(0, ballPrefabs.Length);
 
-        IEnumerator WaitRandomSeconds()
-        {
-            yield return new WaitForSeconds(Random.Range(0f, 2.0f));
-            // instantiate ball at random spawn location
-            Instantiate(ballPrefabs[spawnIndex], spawnPosition, ballPrefabs[spawnIndex].transform.rotation);
-        }
+        Instantiate(ballPrefabs[spawnIndex], spawnPosition, ballPrefabs[spawnIndex].transform.rotation);
 
-        StartCoroutine(WaitRandomSeconds());
+        Invoke(nameof(SpawnRandomBall), Random.Range(spawnIntervalMin, spawnIntervalMax));
     }
 
 }
